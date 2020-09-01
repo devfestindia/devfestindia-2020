e1df65cad3e37cbb78d4c7fc9ee6606985b59116
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-09-01 05:43:23 +0530

    follow us UI updated

--- src/components/home/follow.vue
+++ src/components/home/follow.vue
@@ -1,55 +1,57 @@
 <template>
-  <v-container>
-    <v-row>
+  <v-container fluid>
+    <v-row justify="center" align="center">
       <v-col>
-        <h1 class="google-font">Follow Us</h1>
-        <p class="google-font">
-          Follow us on Social Media and keep an eye on
+        <h1 class="google-font" style="color:#0005DF;">
+          Follow Us
+          <small class="mr-5 ml-1">-</small>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                text
+                icon
+                x-large
+                v-on="on"
+                rel="noreferrer"
+                href="https://twitter.com/DevfestIndia"
+                target="_blank"
+                slot="activator"
+              >
+                <v-icon>mdi-twitter</v-icon>
+              </v-btn>
+            </template>
+            <span>Twitter</span>
+          </v-tooltip>
+
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                text
+                icon
+                x-large
+                v-on="on"
+                rel="noreferrer"
+                href="https://www.youtube.com/channel/UCJsZoFDt7rfxPBI1esak8JQ/"
+                target="_blank"
+                slot="activator"
+              >
+                <v-icon>mdi-youtube</v-icon>
+              </v-btn>
+            </template>
+            <span>YouTube</span>
+          </v-tooltip>
+        </h1>
+        <p class="google-font mb-2">
+          Let's get connected so that we can reach out to each other via a simple notification.
+          <br />Stake out
           <a
             href="https://twitter.com/DevFestIndia"
             rel="noreferrer"
             target="_blank"
             style="color:#0005DF;text-decoration:none"
-          >#DevFestIndia
-          </a>
-          for the latest updates and news about the event.
+          >#DevFestIndia</a>
+          for the updates and news.
         </p>
-
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              text
-              icon
-              x-large
-              v-on="on"
-              rel="noreferrer"
-              href="https://twitter.com/DevfestIndia"
-              target="_blank"
-              slot="activator"
-            >
-              <v-icon>mdi-twitter</v-icon>
-            </v-btn>
-          </template>
-          <span>Twitter</span>
-        </v-tooltip>
-
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              text
-              icon
-              x-large
-              v-on="on"
-              rel="noreferrer"
-              href="https://www.youtube.com/channel/UCJsZoFDt7rfxPBI1esak8JQ/"
-              target="_blank"
-              slot="activator"
-            >
-              <v-icon>mdi-youtube</v-icon>
-            </v-btn>
-          </template>
-          <span>YouTube</span>
-        </v-tooltip>
       </v-col>
     </v-row>
   </v-container>
