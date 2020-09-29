233fa39fda7a61eec8fa0079e98951d2dba1d75a
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-09-29 23:45:47 +0530

    push notification initial files

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
-
-import firebase from 'firebase/app';
-import 'firebase/auth';   
-import 'firebase/firestore';
-import 'firebase/performance'
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/performance";
+import "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA6qChimnN-IuqH--uKK09_Xhg38EP0YBg",
@@ -11,13 +11,20 @@ const firebaseConfig = {
   projectId: "devfestindia2020",
   storageBucket: "devfestindia2020.appspot.com",
   messagingSenderId: "394032196909",
-  appId: "1:394032196909:web:e9bcb020572abcd64e77a4"
+  appId: "1:394032196909:web:e9bcb020572abcd64e77a4",
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().enablePersistence({synchronizeTabs:!0}).catch(()=>{console.warn("DB offline support not available")})
-export default{
+firebase
+  .firestore()
+  .enablePersistence({ synchronizeTabs: !0 })
+  .catch(() => {
+    console.warn("DB offline support not available");
+  });
+export default {
   firestore: firebase.firestore(),
-  auth:firebase.auth(),
-  firebase:firebase,
+  auth: firebase.auth(),
+  firebase: firebase,
+  notificationSupported: firebase.messaging.isSupported(),
+  messaging: firebase.messaging.isSupported() ? firebase.messaging() : null,
 };
