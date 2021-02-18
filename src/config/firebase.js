a627157bb2ad263b556e6a5b8aed54b614291301
Vrijraj Singh <vrijraj2396@gmail.com>
2021-02-18 13:03:40 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,7 @@ import "firebase/performance";
 import "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6qChimnN-IuqH--uKK09_Xhg38EP0YBg",
-  authDomain: "devfestindia2020.firebaseapp.com",
-  databaseURL: "https://devfestindia2020.firebaseio.com",
-  projectId: "devfestindia2020",
-  storageBucket: "devfestindia2020.appspot.com",
-  messagingSenderId: "394032196909",
-  appId: "1:394032196909:web:e9bcb020572abcd64e77a4",
+  // config firebase,
 };
 
 firebase.initializeApp(firebaseConfig);
